0ba60009265e0b3de2f7d6c17d4b21835d23fd00
Parker Nilson <p_nilson@apple.com>
2025-09-23 21:01:01 -0700

    Initial Commit

---
+++ RenecitoPOC/RenecitoPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenecitoPOCApp.swift
+//  RenecitoPOC
+//
+//  Created by Parker Nilson on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct RenecitoPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RenecitoPOCTests/RenecitoPOCTests.swift
@@ -0,0 +1,17 @@
+//
+//  RenecitoPOCTests.swift
+//  RenecitoPOCTests
+//
+//  Created by Parker Nilson on 9/23/25.
+//
+
+import Testing
+@testable import RenecitoPOC
+
+struct RenecitoPOCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
